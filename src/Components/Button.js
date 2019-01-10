ca50de28dc0a99216b4d25565eabeeb950dffc1c
Heath Blandford <heathblandford@Heaths-MacBook-Pro.local>
2019-01-09 19:10:53 -0500

    updating formatting on cards to look good for mobile

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
 import '../Stylizers/Buttons.css';
 
 class Buttons extends Component {
     render(){
         return (
             <div className="buttons">
-                <Button variant="contained" color="primary" id="disappointment">disappointing</Button>
-                <Button variant="contained" color="secondary" id="humblebrag">humblebrag</Button>
+                <Button variant="contained" color="primary" id="disappointment" className="upDownButton">disappointing</Button>
+                <Button variant="contained" color="secondary" id="humblebrag" className="upDownButton">humblebrag</Button>
             </div>
         );
     }
