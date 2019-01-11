24ca5bfb06597fe4540ca6764d1bf80ec166ab5a
Heath Blandford <heathblandford@Heaths-MacBook-Pro.local>
2019-01-10 19:05:55 -0500

    got button onclick to add and subtract from post total score need to figure out how to display this properly tho

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,10 +4,22 @@ import '../Stylizers/Buttons.css';
 
 class Buttons extends Component {
     render(){
+        let score = 0;
+        function upvote(){
+            score += 1;
+            console.log(score)
+            return score;
+        }
+
+        function downvote(){
+            score -= 1;
+            console.log(score);
+            return score;
+        }
         return (
             <div className="buttons">
-                <Button variant="contained" color="primary" id="disappointment" className="upDownButton">disappointing</Button>
-                <Button variant="contained" color="secondary" id="humblebrag" className="upDownButton">humblebrag</Button>
+                <Button variant="contained" color="primary" id="disappointment" className="upDownButton" onClick={upvote}>disappointing</Button>
+                <Button variant="contained" color="secondary" id="humblebrag" className="upDownButton" onClick={downvote}>humblebrag</Button>
             </div>
         );
     }
