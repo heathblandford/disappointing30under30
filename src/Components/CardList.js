8b13e4cc9c42519d1c3a8a6df4de82f49f64a666
Heath Blandford <heathblandford@Heaths-MacBook-Pro.local>
2019-01-11 21:55:44 -0500

    created card list js file and commented out everything it's going to be creating cards based on api data

---
+++ src/Components/CardList.js
@@ -0,0 +1,17 @@
+/*
+    Including the fetch code here.
+    this is going to be the way i fetch data from my api but i need
+    to work out the hoisting issue with the disappointmentList
+    Do I just need to do everything I would typically do in side the .then method???
+*/
+
+
+// let disappointmentList;
+// fetch("https://frozen-plateau-56176.herokuapp.com")
+//     .then(res => {
+//         return res.json()
+//     })
+//     .then(myJson => {
+//         disappointmentList = myJson;
+//     })
+//     console.log(disappointmentList);
