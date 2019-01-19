c5c304d40c4932f2b26e46eea2f80a3a15561a97
Heath Blandford - Home PC <heath.blandford@gmail.com>
2019-01-18 20:08:27 -0500

    change size of social buttons update modal style

--- src/Container/App.js
+++ src/Container/App.js
@@ -5,13 +5,19 @@ import '../Assets/plusButton.svg';
 import Modal from 'react-modal';
 
 const customStyles = { //this is for the modal
+	overlay: {
+		backgroundColor       : 'rgba(0,0,0,0.9)'
+	}, 
 	content : {
 		top                   : '50%',
 		left                  : '50%',
 		right                 : 'auto',
 		bottom                : 'auto',
 		marginRight           : '-50%',
-		transform             : 'translate(-50%, -50%)'
+		transform             : 'translate(-50%, -50%)',
+		height								: '400px',
+		width									: '300px',
+		borderRadius					: '15px',
 	}
 };
 class App extends Component {
@@ -19,7 +25,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-		data: [],
+			data: [],
     	loading: true,
     	modalIsOpen: false,
 	}
@@ -36,25 +42,26 @@ openModal() {
 
 afterOpenModal() {
 	// references are now sync'd and can be accessed.
-	this.subtitle.style.color = '#f00';
+	this.subtitle.style.color = '#b96fd6';
+	this.subtitle.style.textAlign = 'center';
 }
 
 closeModal() {
 	this.setState({modalIsOpen: false});
 }
 
-handleSubmit = (e) => {
-	document.submitYourOwn.submit();
-	setTimeout(() => {this.setState({modalIsOpen: false})}, 500);
-	e.preventDefault();
+handleSubmit = (e) => { //this is a "manual" submit instead of just using standard HTML type submit
+	document.submitYourOwn.submit(); //this is how we submit
+	setTimeout(() => {this.setState({modalIsOpen: false})}, 500); // set time out so that the web page had time to submit the form
+	e.preventDefault(); //prevent default submit option. ez pz
 }
 
 componentDidMount() {
-	//fetch api data
+		//fetch api data
 	  fetch('https://frozen-plateau-56176.herokuapp.com')
     .then(response => response.json())
-	// { data } is object shorthand. this.setState() expects an object to return an array
-	.then(data => this.setState({ data, loading: false }));
+		// { data } is object shorthand. this.setState() expects an object to return an array
+		.then(data => this.setState({ data, loading: false }));
 
 		setTimeout(() => {
 			this.setState({ loading: false})
@@ -65,34 +72,34 @@ componentDidMount() {
     const { data } = this.state;
 	  const { loading } = this.state;
 
-	if(loading) { // if your component doesn't have to wait for an async action, remove this block
+		if(loading) { // if your component doesn't have to wait for an async action, remove this block
     	return null; // render null when app is not ready
-	}
+		}
 
     return (
       <div className="App">
         <header className="App-header">
-            <h1>Disappointing 30 under 30</h1>
-            <p>A place to remind your parents how much of a let down you are</p>
-				<button onClick={this.openModal}>Open Modal</button>
-			<Modal
-				isOpen={this.state.modalIsOpen}
-				onAfterOpen={this.afterOpenModal}
-				onRequestClose={this.closeModal}
-				style={customStyles}
-				// contentLabel="Example Modal"
-			>
+							<h1>Disappointing 30 under 30</h1>
+							<p>A place to remind your parents how much of a let down you are</p>
+					<button onClick={this.openModal}>Open Modal</button>
+					<Modal
+						isOpen={this.state.modalIsOpen}
+						onAfterOpen={this.afterOpenModal}
+						onRequestClose={this.closeModal}
+						style={customStyles}
+						// contentLabel="Example Modal"
+					>
 
-			<h2 ref={subtitle => this.subtitle = subtitle}>Add Your Own</h2>
-			<form action="https://frozen-plateau-56176.herokuapp.com" method="post" target="dummyframe" onSubmit={this.handleSubmit} name="submitYourOwn">
-				<input type="text" name="first_name" placeholder="First Name"/><br/>
-				<input type="text" name="last_name" placeholder="Last Name"/><br/>
-				<input type="number" name="age" placeholder="29"/><br/>
-				<textarea cols="50" rows="5" name="disappointment" placeholder="Type your disappointment here!"/><br />
-				<input type="submit" value="SEND IT" onSubmit={this.handleSubmit}/>
-			</form>
-			<button onClick={this.closeModal}>Cancel</button>
-			</Modal>
+						<h1 ref={subtitle => this.subtitle = subtitle}>Add Your Own</h1>
+						<form action="https://frozen-plateau-56176.herokuapp.com" method="post" target="dummyframe" onSubmit={this.handleSubmit} name="submitYourOwn">
+							<input type="text" name="first_name" placeholder="First Name"/><br/>
+							<input type="text" name="last_name" placeholder="Last Name"/><br/>
+							<input type="number" name="age" placeholder="29"/><br/>
+							<textarea cols="25" rows="5" name="disappointment" placeholder="Type your disappointment here!"/><br />
+						</form>
+							<input type="submit" value="SEND IT" onSubmit={this.handleSubmit}/>
+							<button onClick={this.closeModal}>Cancel</button>
+					</Modal>
         </header>
 
 
