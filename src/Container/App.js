c03bc10024691b9d6067fc89fc80bbc88f75c4f6
Heath Blandford - Home PC <heath.blandford@gmail.com>
2019-01-19 13:45:42 -0500

    prettier update

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,102 +1,117 @@
-import React, { Component } from 'react';
-import CardList from '../Components/CardList';
-import '../Stylizers/App.css';
-import '../Assets/plusButton.svg';
-import Modal from 'react-modal';
+import React, { Component } from "react";
+import CardList from "../Components/CardList";
+import "../Stylizers/App.css";
+import "../Assets/plusButton.svg";
+import Modal from "react-modal";
 
-const customStyles = { //this is for the modal
-	content : {
-		top                   : '50%',
-		left                  : '50%',
-		right                 : 'auto',
-		bottom                : 'auto',
-		marginRight           : '-50%',
-		transform             : 'translate(-50%, -50%)'
-	}
+const customStyles = {
+  //this is for the modal
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
 };
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-		data: [],
-    	loading: true,
-    	modalIsOpen: false,
-	}
+      data: [],
+      loading: true,
+      modalIsOpen: false
+    };
 
-	  this.openModal = this.openModal.bind(this);
+    this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
-		this.closeModal = this.closeModal.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-};
+    this.closeModal = this.closeModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-openModal() {
-    this.setState({modalIsOpen: true});
-}
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
 
-afterOpenModal() {
-	// references are now sync'd and can be accessed.
-	this.subtitle.style.color = '#f00';
-}
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = "#f00";
+  }
 
-closeModal() {
-	this.setState({modalIsOpen: false});
-}
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
 
-handleSubmit(event) {
-	event.preventDefault();
-	this.setState({modalIsOpen: false});
-}
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ modalIsOpen: false });
+  }
 
-componentDidMount() {
-	//fetch api data
-	  fetch('https://frozen-plateau-56176.herokuapp.com')
-    .then(response => response.json())
-		// { data } is object shorthand. this.setState() expects an object to return an array
-		.then(data => this.setState({ data }));
+  componentDidMount() {
+    //fetch api data
+    fetch("https://frozen-plateau-56176.herokuapp.com")
+      .then(response => response.json())
+      // { data } is object shorthand. this.setState() expects an object to return an array
+      .then(data => this.setState({ data }));
 
-		setTimeout(() => {
-			this.setState({ loading: false})
-		}, 1500);
-}
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 1500);
+  }
 
   render() {
     const { data } = this.state;
-	  const { loading } = this.state;
+    const { loading } = this.state;
 
-	if(loading) { // if your component doesn't have to wait for an async action, remove this block
-    	return null; // render null when app is not ready
-	}
+    if (loading) {
+      // if your component doesn't have to wait for an async action, remove this block
+      return null; // render null when app is not ready
+    }
 
     return (
       <div className="App">
         <header className="App-header">
-            <h1>Disappointing 30 under 30</h1>
-            <p>A place to remind your parents how much of a let down you are</p>
-				<button onClick={this.openModal}>Open Modal</button>
-			<Modal
-				isOpen={this.state.modalIsOpen}
-				onAfterOpen={this.afterOpenModal}
-				onRequestClose={this.closeModal}
-				style={customStyles}
-				// contentLabel="Example Modal"
-			>
-
-			<h2 ref={subtitle => this.subtitle = subtitle}>Add Your Own</h2>
-			<form action="https://frozen-plateau-56176.herokuapp.com" method="post" target="dummyframe" > {/* this closes the modal fine but doesn't submit the form */}
-				<input type="text" name="first_name" placeholder="First Name"/><br/>
-				<input type="text" name="last_name" placeholder="Last Name"/><br/>
-				<input type="number" name="age" placeholder="29"/><br/>
-				<textarea cols="50" rows="5" name="disappointment" placeholder="Type your disappointment here!"/><br />
-				<input type="submit" value="SEND IT" onSubmit={this.handleSubmit}/>
-			</form>
-			<button onClick={this.closeModal}>Cancel</button>
-			</Modal>
+          <h1>Disappointing 30 under 30</h1>
+          <p>A place to remind your parents how much of a let down you are</p>
+          <button onClick={this.openModal}>Open Modal</button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            // contentLabel="Example Modal"
+          >
+            <h2 ref={subtitle => (this.subtitle = subtitle)}>Add Your Own</h2>
+            <form
+              action="https://frozen-plateau-56176.herokuapp.com"
+              method="post"
+              target="dummyframe"
+              onSubmit={this.handleSubmit}
+            >
+              {/* this closes the modal fine but doesn't submit the form */}
+              <input type="text" name="first_name" placeholder="First Name" />
+              <br />
+              <input type="text" name="last_name" placeholder="Last Name" />
+              <br />
+              <input type="number" name="age" placeholder="29" />
+              <br />
+              <textarea
+                cols="50"
+                rows="5"
+                name="disappointment"
+                placeholder="Type your disappointment here!"
+              />
+              <br />
+              <button type="submit">SEND IT</button>
+            </form>
+            <button onClick={this.closeModal}>Cancel</button>
+          </Modal>
         </header>
 
-
         <main>
-            <CardList data={ data }/>
+          <CardList data={data} />
         </main>
       </div>
     );
